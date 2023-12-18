61865d99bea99b12a1e1bd35a885a2a61763d934
Mahesh Paliath <mahesh.paliath@live.com>
2023-12-18 12:43:50 -0800

    Basic auth to list Onedrive files

--- src/env/App/App.tsx
+++ src/env/App/App.tsx
@@ -1,18 +1,59 @@
-import type {FC} from 'react';
-
-import {CloudFilePicker} from '@/lib';
 import './index.css';
 
+import type {FC} from 'react';
+import {useState, useEffect} from 'react';
+import {Providers, ProviderState} from '@microsoft/mgt-element';
+import {Login} from '@microsoft/mgt-react';
+
+import {CloudFilePicker} from '@/lib';
+
 const App: FC = () => {
+    const [accessToken, setAccessToken] = useState<string>('');
+
+    const [isSignedIn, setIsSignedIn] = useState(false);
+
+    useEffect(() => {
+        const updateState = () => {
+            const provider = Providers.globalProvider;
+            setIsSignedIn(provider && provider.state === ProviderState.SignedIn);
+        };
+
+        Providers.onProviderUpdated(updateState);
+        updateState();
+
+        return () => {
+            Providers.removeProviderUpdatedListener(updateState);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (Providers.globalProvider.state === ProviderState.SignedIn) {
+            Providers.globalProvider
+                .getAccessToken({scopes: ['files.readwrite', 'offline_access']})
+                .then(token => {
+                    setAccessToken(token);
+                })
+                .catch(error => {
+                    console.error('Error getting access token:', error);
+                });
+        } else if (Providers.globalProvider.state === ProviderState.SignedOut) {
+            setAccessToken('');
+        }
+    }, [isSignedIn]);
+
     return (
-        <div>
-            Hello
-            <CloudFilePicker
-                accessToken=""
-                onConfirmSelection={() => {
-                    alert('');
-                }}
-            />
+        <div className="app">
+            <header>
+                <Login />
+            </header>
+            {accessToken && (
+                <CloudFilePicker
+                    accessToken={accessToken ?? ''}
+                    onConfirmSelection={() => {
+                        alert('');
+                    }}
+                />
+            )}
         </div>
     );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Providers} from '@microsoft/mgt-element';
+import {Msal2Provider} from '@microsoft/mgt-msal2-provider';
 
 import App from '@/env/App/App.tsx';
+
+Providers.globalProvider = new Msal2Provider({clientId: '5cf21436-2a20-447b-b222-77a1326409bb'});
 
 // eslint-disable-next-line ssr-friendly/no-dom-globals-in-module-scope
 ReactDOM.createRoot(document.getElementById('root')!).render(
