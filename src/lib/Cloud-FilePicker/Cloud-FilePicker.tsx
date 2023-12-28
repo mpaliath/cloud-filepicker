f1e9cdd4f35ef20fc594a87132fdbbb1432caf35
Mahesh Paliath <mahesh.paliath@live.com>
2023-12-28 12:28:43 -0800

    Add breadcrumb

--- src/lib/Cloud-FilePicker/Cloud-FilePicker.tsx
+++ src/lib/Cloud-FilePicker/Cloud-FilePicker.tsx
@@ -25,6 +25,7 @@ export const CloudFilePicker: FC<FileListProps> = props => {
     const [files, setFiles] = useState<File[] | null>(null);
     const [selectedFiles, setSelectedFiles] = useState<string[]>([]);
     const [currentFolder, setCurrentFolder] = useState<string>('me/drive/special/photos');
+    const [breadcrumbs, setBreadcrumbs] = useState<{id: string; name: string}[]>([]);
 
     const fetchFiles = (folder: string) => {
         fetch(`https://graph.microsoft.com/v1.0/${folder}/children`, {
@@ -44,6 +45,11 @@ export const CloudFilePicker: FC<FileListProps> = props => {
         fetchFiles(currentFolder);
     }, [props.accessToken, currentFolder]);
 
+    useEffect(() => {
+        // Set the initial breadcrumb to represent the root folder
+        setBreadcrumbs([{id: 'me/drive/special/photos', name: 'Root'}]);
+    }, []); // empty dependency array for initial mount
+
     const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setSelectedFiles(prevSelectedFiles =>
             event.target.checked
@@ -56,22 +62,62 @@ export const CloudFilePicker: FC<FileListProps> = props => {
         props.onConfirmSelection(selectedFiles);
     };
 
-    const handleFolderClick = (folderId: string) => {
+    const handleFolderClick = (folderId: string, folderName: string) => {
         setCurrentFolder(`me/drive/items/${folderId}`);
+        if (currentFolder === 'me/drive/special/photos') {
+            // Navigating from the root, replace the breadcrumb with the root folder
+            setBreadcrumbs([{id: 'me/drive/special/photos', name: 'Root'}]);
+            setBreadcrumbs(prevBreadcrumbs => [...prevBreadcrumbs, {id: folderId, name: folderName}]);
+        } else {
+            // Navigating into a subfolder, append to the existing breadcrumb
+            setBreadcrumbs(prevBreadcrumbs => [...prevBreadcrumbs, {id: folderId, name: folderName}]);
+        }
+    };
+
+    const handleBreadcrumbClick = (index: number) => {
+        const newBreadcrumbs = breadcrumbs.slice(0, index + 1);
+        const lastBreadcrumb = newBreadcrumbs[newBreadcrumbs.length - 1];
+
+        if (lastBreadcrumb.name == 'Root') {
+            setCurrentFolder(`me/drive/special/photos`);
+            setBreadcrumbs([{id: 'me/drive/special/photos', name: 'Root'}]);
+        } else {
+            setCurrentFolder(`me/drive/items/${lastBreadcrumb.id}`);
+            setBreadcrumbs(newBreadcrumbs);
+        }
     };
 
     return (
-        <div style={{width: '100%'}}>
-            <div style={{display: 'flex', flexWrap: 'wrap'}}>
+        <div style={{maxWidth: '100%', overflowX: 'hidden'}}>
+            <div style={{marginBottom: '10px'}}>
+                {breadcrumbs.map((breadcrumb, index) => (
+                    <span key={breadcrumb.id}>
+                        {index !== 0 && ' > '}
+                        <button onClick={() => handleBreadcrumbClick(index)}>{breadcrumb.name}</button>
+                    </span>
+                ))}
+            </div>
+
+            <div style={{display: 'flex', flexWrap: 'wrap', overflowY: 'auto', maxHeight: '400px'}}>
                 {files?.map(file => (
-                    <div key={file.id} style={{margin: '10px', cursor: 'pointer'}}>
+                    <div
+                        key="folderContent"
+                        style={{margin: '10px', cursor: 'pointer', textAlign: 'center', maxWidth: '140px'}}>
                         {file.folder ? (
-                            <img
-                                style={{width: '100px', height: '100px'}}
-                                src={folderImage}
-                                alt="folder"
-                                onClick={() => handleFolderClick(file.id)}
-                            />
+                            <div>
+                                <img
+                                    style={{width: '100px', height: '100px'}}
+                                    src={folderImage}
+                                    alt={file.name}
+                                    onClick={() => handleFolderClick(file.id, file.name)}
+                                />
+                                <br />
+                                <label
+                                    style={{overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap'}}
+                                    htmlFor={file.id}>
+                                    {file.name}
+                                </label>
+                            </div>
                         ) : (
                             <>
                                 <img
@@ -79,9 +125,10 @@ export const CloudFilePicker: FC<FileListProps> = props => {
                                     alt={file.name}
                                     style={{width: '100px', height: '100px'}}
                                 />
+                                {/* <br />
+                            <label style={{ display:'block', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }} htmlFor={file.id}>{file.name}</label> */}
                                 <br />
                                 <input type="checkbox" id={file.id} onChange={handleCheckboxChange} />
-                                {/* <label htmlFor={file.id}>{file.name}</label> */}
                             </>
                         )}
                     </div>
